b1930473464a50182d557799ad4a017e6ada0f55
slashburygin <slashburygin@gmail.com>
2026-05-18 23:48:39 +0300

    update messages on install and clear

--- exordos/cmd/em/elements/commands.py
+++ exordos/cmd/em/elements/commands.py
@@ -251,12 +251,14 @@ def upgrade_manifest(
     all_installed_elements = required_elements.copy()
     all_installed_elements.append(manifest["name"])
     click.echo(
-        f"The following elements will be {'installed' if install_only else 'upgraded'}: "
+        f"The following elements will be {'installed' if install_only else 'upgraded'} first: "
         f"{click.style(', '.join(all_installed_elements), fg='green')}"
     )
 
     while required_elements:
         requirement = required_elements.pop()
+        if requirement not in installed_elements:
+            click.echo(f"The following dependency element will be installed: {requirement}")
         req_manifest = repo_lib.download_manifest(repository, requirement)
 
         # Determine requirements for the element
@@ -520,6 +522,8 @@ def clear(ctx: click.Context, y: bool) -> None:  # pragma: no cover
             click.echo(f"Skipping base element {entity['name']}")
             continue
         if y or Confirm.ask(f"Do you want to uninstall {entity['name']}?"):
+            uninstalled_name = f"{entity['name']} ({entity['version']})"
+            click.echo(f"Uninstalling element {click.style(uninstalled_name, fg='green')}")
             base_client.delete_entity(client, ENTITY_COLLECTION, entity["uuid"])
     return None
 
